992cc6813e4b1afe9a5657e39804f2877c503399
MamieJatthews <jama.a.matthews@gmail.com>
2023-10-10 09:26:54 +1300

    ok

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
 let sourceImg=null;
 let maskImg=null;
-let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_4.jpg";
-let maskFile   = "mask_4.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
 let outputFile = "output_1.png";
 
 function preload() {
@@ -16,63 +15,51 @@ function setup () {
   let main_canvas = createCanvas(1920, 1080);
   main_canvas.parent('canvasContainer');
 
-  // imageMode(CENTER);
+  imageMode(CENTER);
   noStroke();
-  background(128, 128, 128);
+  background(0, 0, 128);
   sourceImg.loadPixels();
   maskImg.loadPixels();
-  // image(sourceImg, 0, 0, width, height);
+  colorMode(HSB);
 }
 
-function owl(x, y, g, s) {
-  push();
-  translate(x, y);
-  scale(s);  // Set the createCanvas
-  stroke(g); // Set the gray value
-  strokeWeight(70);
-  line(0, -35, 0, -65); // Body
-  noStroke();
-  fill(255-g);
-  ellipse(-17.5, -65, 35, 35); // Left eye dome
-  ellipse(17.5, -65, 35, 35);  // Right eye dome
-  arc(0, -65, 70, 70, 0, PI);  // Chin
-  fill(g);
-  ellipse(-14, -65, 8, 8);  // Left eye
-  ellipse(14, -65, 8, 8);   // Right eye
-  quad(0, -58, 4, -51, 0, -44, -4, -51); // Beak
-  pop();
-}
+//let X_STOP = 640;
+//let Y_STOP = 480;
+let X_STOP = 1920;
+let Y_STOP = 1080;
+let OFFSET = 20;
 
+let renderCounter=0;
 function draw () {
-  for(let i=0;i<10000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let scale = 0.1;
-    // if(mask[0] > 128) {
-    //   scale = 0.5;
-    // }
-    // else {
-    //   scale = 0.1;
-    // }
-    fill(pix);
-    stroke(pix);
-    let pointSize = 20;
-    if(mask[0] > 128) {
-      strokeWeight(2);
-      //line(x-1, y, x+1, y);
-    }
-    else {
-      strokeWeight(1);
-      line(x, y-50, x, y+50);
-    }
+  angleMode(DEGREES);
+  let num_lines_to_draw = 40;
+  // get one scanline
+  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
+    for(let i=0; i<X_STOP; i++) {
+      colorMode(RGB);
+      let mask = maskImg.get(i, j);
+      if (mask[1] > 128) {
+        pix = sourceImg.get(i, j);
+      }
+      else {
+        let wave = sin(j*8);
+        let slip = map(wave, -1, 1, -OFFSET, OFFSET);
+        pix = sourceImg.get(i+slip, j);
 
-    // ellipse(x, y, pointSize, pointSize);
-    // owl(x, y, pix[1], scale);
+        // let brt = map(wave, -1, 1, 0, 255);
+        // for(let c=0; c<3; c++) {
+        //   pix[c] = brt;
+        // }
+      }
+
+      set(i, j, pix);
+    }
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  renderCounter = renderCounter + num_lines_to_draw;
+  updatePixels();
+
+  // print(renderCounter);
+  if(renderCounter > Y_STOP) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
