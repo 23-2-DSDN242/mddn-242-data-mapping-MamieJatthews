6546e81b42b9c38e549910c40782be007ac79a2d
MamieJatthews <jama.a.matthews@gmail.com>
2023-10-10 13:13:32 +1300

    ok

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let sourceImg=null;
 let maskImg=null;
+let renderCounter=0;
+let curLayer = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
+let sourceFile = "input_5.jpg";
+let maskFile   = "mask_5.png";
 let outputFile = "output_1.png";
 
 function preload() {
@@ -29,35 +31,63 @@ let X_STOP = 1920;
 let Y_STOP = 1080;
 let OFFSET = 20;
 
-let renderCounter=0;
 function draw () {
-  angleMode(DEGREES);
   let num_lines_to_draw = 40;
-  // get one scanline
-  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
-    for(let i=0; i<X_STOP; i++) {
-      colorMode(RGB);
-      let mask = maskImg.get(i, j);
-      if (mask[1] > 128) {
-        pix = sourceImg.get(i, j);
-      }
-      else {
-        let wave = sin(j*8);
-        let slip = map(wave, -1, 1, -OFFSET, OFFSET);
-        pix = sourceImg.get(i+slip, j);
+  if (curLayer == 0) {
 
-        // let brt = map(wave, -1, 1, 0, 255);
-        // for(let c=0; c<3; c++) {
-        //   pix[c] = brt;
-        // }
-      }
+      angleMode(DEGREES);
+      let num_lines_to_draw = 40;
+      // get one scanline
+      for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
+        for(let i=0; i<X_STOP; i++) {
+          colorMode(RGB);
+          let mask = maskImg.get(i, j);
+          if (mask[1] > 128) { 
+            pix = sourceImg.get(i, j);
+            pix[0] = map(30, 0, 100, pix[0], 255)
+            pix[1] = map(30, 0, 100, pix[1], 255)
+            set(i, j, pix);
+          }
+          else {  
+            // let wave = sin(j*8);
+            // let slip = map(wave, -1, 1, -OFFSET, OFFSET);
+            // pix = sourceImg.get(i+slip, j);
+          }
 
-      set(i, j, pix);
+        }
+      }
     }
-  }
+    else if (curLayer == 1) {
+
+      angleMode(DEGREES);
+      let num_lines_to_draw = 40;
+      // get one scanline
+      for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
+        for(let i=0; i<X_STOP; i++) {
+          colorMode(RGB);
+          let mask = maskImg.get(i, j);
+          if (mask[1] > 128) { 
+            // pix = sourceImg.get(i, j);
+            // pix[0] = map(30, 0, 100, pix[0], 255)
+            // pix[1] = map(30, 0, 100, pix[1], 255)
+          }
+          else {  
+            let wave = sin(j*8);
+            let slip = map(wave, -1, 1, -OFFSET, OFFSET);
+            pix = sourceImg.get(i+slip, j);
+            set(i, j, pix);
+          }
+
+        }
+      }
+    }
   renderCounter = renderCounter + num_lines_to_draw;
   updatePixels();
 
+  if(curLayer == 0 && renderCounter > 1080) {
+    curLayer = 1;
+    renderCounter = 0;
+  }
   // print(renderCounter);
   if(renderCounter > Y_STOP) {
     console.log("Done!")
